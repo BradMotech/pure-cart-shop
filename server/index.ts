9f6e3173e2befe6ac25a3f5e27a65c841c45e075
Bradley Mamanyoha <b.mamanyoha@omniplan.nl>
2025-09-01 12:16:53 +0200

    fix:replit

--- server/index.ts
+++ server/index.ts
@@ -61,6 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
+  
   app.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
